9870524af52cdc5e97e19da86d08f94982c5f50e
Tomasz Pytel <tompytel@gmail.com>
2025-02-02 08:17:48 -0500

    tweak aststr

--- src/fst/util.py
+++ src/fst/util.py
@@ -6,19 +6,18 @@ __all__ = ['aststr', 'astfield', 'get_node_field']
 
 
 class aststr(str):
-    """Easy mapping between char and encoded byte indices (including 1 past last valid unit). Negative index conversions
-    start with this nonexistent one past character so give length of string in chars when converting -1. Last index of
-    an actual character or utf8 codeword is at -2."""
+    """Easy mapping between char and encoded byte indices (including 1 past last valid unit). Only positive indexes."""
 
-    __c2b: array  # character to byte indices
-    __b2c: array  # byte to character indices
+    _c2b: array  # character to byte indices
+    _b2c: array  # byte to character indices
 
-    _i2i = lambda idx: idx
+    _i2i_same = lambda idx: idx
 
     def __new__(cls, s: str) -> 'aststr':
         return s if s.__class__ is aststr else str.__new__(cls, s)
 
-    def __empty_array(self, len_array: int, highest_value: int) -> array:
+    @staticmethod
+    def _make_array(len_array: int, highest_value: int) -> array:
         if highest_value < 0x100:
             return array('B', b'\x00' * (len_array + 1))
         if highest_value < 0x10000:
@@ -28,16 +27,16 @@ class aststr(str):
 
         return array('Q', b'\x00\x00\x00\x00\x00\x00\x00\x00' * (len_array + 1))
 
-    def _c2b(self, idx):
-        return self.__c2b[idx]
+    def c2b_lookup(self, idx):
+        return self._c2b[idx]
 
     def c2b(self, idx: int) -> int:
         if (lc := len(self)) == (lb := len(self.encode())):
-            self.c2b = self.b2c = aststr._i2i
+            self.c2b = self.b2c = aststr._i2i_same
 
             return idx
 
-        c2b = self.__c2b = self.__empty_array(lc, lb)
+        c2b = self._c2b = self._make_array(lc, lb)
         j   = 0
 
         for i, c in enumerate(self):
@@ -45,36 +44,42 @@ class aststr(str):
             j      += len(c.encode())
 
         c2b[-1]  = j
-        self.c2b = self._c2b
+        self.c2b = self.c2b_lookup
 
         return c2b[idx]
 
-    def _b2c(self, idx):
-        return self.__b2c[idx] or (idx and (None if idx > -len(self.__b2c) else 0))
+    def b2c_lookup(self, idx):
+        return self._b2c[idx]
 
     def b2c(self, idx: int) -> int:
         if (lb := self.c2b(lc := len(self))) == lc:
-            return idx  # no chars > '\x7f' so funcs are set to `_i2i` identity
+            return idx  # no chars > '\x7f' so funcs are `_i2i_same` identity
 
-        b2c = self.__b2c = self.__empty_array(lb, lc)
+        b2c = self._b2c = self._make_array(lb, lc)
 
-        for i, j in enumerate(self.__c2b):
+        for i, j in enumerate(self._c2b):
             b2c[j] = i
 
-        self.b2c = self._b2c
+        k = 0
+
+        for i, j in enumerate(b2c):  # set off-boundary utf8 byte indices for safety
+            if j:
+                k = j
+            else:
+                b2c[i] = k
+
+        self.b2c = self.b2c_lookup
 
         return b2c[idx]
 
-    def clear(self):
+    def clear_cache(self):
         try:
-            del self.c2b
-            del self.__c2b
+            del self.c2b, self._c2b
         except AttributeError:
             pass
 
         try:
-            del self.b2c
-            del self.__b2c
+            del self.b2c, self._b2c
         except AttributeError:
             pass
 
